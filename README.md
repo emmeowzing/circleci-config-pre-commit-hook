89808a8d9c360ae2b7e7f14d595e49246a78ea8e
Brandon Doyle <brandon.doyle@hqo.co>
2022-05-08 14:26:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## CircleCI config validation pre-commit hook
+
+This CircleCI config validation [pre-commit](https://github.com/pre-commit/pre-commit) hook is intended to be used in conjunction with the [`dynamic-continuation`](https://github.com/bjd2385/dynamic-continuation-orb) orb (although this is not required). It extends the usual `circleci config validate` step by further validating a reduced config, excluding the default config, which dynamic-continuation would normally submit from a setup workflow.
+
+### Prerequisites
+
+You must have the [`circleci`](https://circleci.com/docs/2.0/local-cli/) CLI installed, as well as [`yq`](https://github.com/mikefarah/yq#install), which is used to reduce the config files.
+
+#### Example `.pre-commit-config.yaml`
+
+```yaml
+repos:
+...
+- repo: https://github.com/bjd2385/circleci-config-pre-commit-hook
+    rev: v1.0.0
+    hooks:
+      - id: circleci-config-validate
+```
---
+++ src/circleci-config-validate.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+
+set -e
+
+if [[ -n $CIRCLECI ]]; then
+    echo "Circleci environment detected, skipping validation."
+    exit 0
+fi
+
+if ! command -v circleci &>/dev/null; then
+    echo "Circleci CLI could not be found. Install the latest CLI version https://circleci.com/docs/2.0/local-cli/#installation"
+    exit 1
+fi
+
+if ! command -v yq &>/dev/null; then
+    echo "yq could not be found. Install the latest CLI version with 'brew install yq'"
+    exit 1
+fi
+
+# Validate the standard CircleCI config.
+if ! eMSG=$(circleci config validate "$@" -c .circleci/config.yml); then
+    echo "CircleCI Configuration Failed Validation."
+    echo "${eMSG}"
+    exit 1
+fi
+
+# Now process the dynamic-continuation generated / reduced configuration file.
+mapfile -t mods < <(find .circleci -type f \( -iname "*.yml" ! -iname "config.yml" \))
+
+if [ "${#mods[@]}" -ne 0 ]; then
+    continueConfig="$(mktemp)"
+    yq ea ". as \$item ireduce ({}; . * \$item)" "${mods[@]}" | tee "$continueConfig"
+    yq -i '.version = 2.1' "$continueConfig"
+    yq 'del(.setup)' "$continueConfig"
+    yq -i '.workflows.version = 2' "$continueConfig"
+
+    if ! reMSG=$(circleci config validate "$@" -c "$continueConfig"); then
+        echo "CircleCI dynamic-continuation configuration failed validation."
+        echo "${reMSG}"
+        exit 1
+    fi
+fi
