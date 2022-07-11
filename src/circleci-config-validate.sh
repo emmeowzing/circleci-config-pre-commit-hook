6f322c596154bce4da039430422a8c89532c6c4d
Brandon Doyle <brandon.doyle@hqo.co>
2022-07-11 14:49:44 -0400

    Fix buggy CircleCI config validate pre-commit hook

--- src/circleci-config-validate.sh
+++ src/circleci-config-validate.sh
@@ -12,31 +12,13 @@ if ! command -v circleci &>/dev/null; then
     exit 1
 fi
 
-if ! command -v yq &>/dev/null; then
-    echo "yq could not be found. Install the latest CLI version with 'brew install yq'"
-    exit 1
-fi
+# Process all config files for independent validity.
+mapfile -t mods < <(find .circleci -type f \( -iname "*.yml" \))
 
-# Validate the standard CircleCI config.
-if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
-    echo "CircleCI Configuration Failed Validation."
-    echo "${eMSG}"
-    exit 1
-fi
-
-# Now process the dynamic-continuation generated / reduced configuration file.
-mapfile -t mods < <(find .circleci -type f \( -iname "*.yml" ! -iname "config.yml" \))
-
-if [ "${#mods[@]}" -ne 0 ]; then
-    continueConfig="$(mktemp)"
-    yq ea ". as \$item ireduce ({}; . * \$item)" "${mods[@]}" | tee "$continueConfig"
-    yq -i '.version = 2.1' "$continueConfig"
-    yq 'del(.setup)' "$continueConfig"
-    yq -i '.workflows.version = 2' "$continueConfig"
-
-    if ! reMSG=$(circleci config validate -c "$continueConfig"); then
-        echo "CircleCI dynamic-continuation configuration failed validation."
+for config in "${mods[@]}"; do
+    if ! reMSG=$(circleci config validate -c "$config"); then
+        printf "CircleCI config file \"%s\" failed validation.\\n" "$config"
         echo "${reMSG}"
         exit 1
     fi
-fi
+done
